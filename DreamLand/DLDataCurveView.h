e19e772345d4d74255da87b64229be0f8eff277a
rickytan <ricky.tan.xin@gmail.com>
2014-03-03 21:27:08 +0800

    add curve view

---
+++ DreamLand/DLDataCurveView.h
@@ -0,0 +1,13 @@
+//
+//  DLDataCurverView.h
+//  DreamLand
+//
+//  Created by ricky on 14-3-3.
+//  Copyright (c) 2014年 ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLDataCurveView : UIView
+@property (nonatomic, retain) NSArray * data;
+@end
---
+++ DreamLand/DLDataCurveView.m
@@ -0,0 +1,37 @@
+//
+//  DLDataCurverView.m
+//  DreamLand
+//
+//  Created by ricky on 14-3-3.
+//  Copyright (c) 2014年 ricky. All rights reserved.
+//
+
+#import "DLDataCurveView.h"
+#import <CorePlot/CorePlot.h>
+
+@implementation DLDataCurveView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    
+}
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    
+}
+
+
+@end
